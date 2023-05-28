fbd1625a521f1d4f38fa9035896b4292284b6191
maria <mariaizabellasda@gmail.com>
2023-05-28 00:11:36 -0300

    alteracao

--- app/src/main/java/com/example/agendaaqui/ConsultaModelo.kt
+++ app/src/main/java/com/example/agendaaqui/ConsultaModelo.kt
@@ -1,3 +1,11 @@
 package com.example.agendaaqui.com.example.agendaaqui
 
-data class ConsultaModelo()
+import android.widget.EditText
+
+data class ConsultaModelo(
+    var susId: String? = null,
+    var susEspecialidade: String? = null,
+    var susMedico: String? = null,
+    var susHorario: String? = null,
+    var susData: String? = null
+)
